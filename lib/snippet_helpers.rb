694f1295b52440d315c02043a0c9dc78ae06587d
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2018-10-23 12:15:19 +0100

    EA1x5Npl: rename xsd files to xml

--- lib/snippet_helpers.rb
+++ lib/snippet_helpers.rb
@@ -1,10 +1,4 @@
 module SnippetHelpers
-  def xsd(name)
-    root = Middleman::Application.root
-    file_path = File.join(root, 'source',"#{name}.xsd")
-    return "```\n#{File.read(file_path)}```"
-  end
-
   def xml(name)
     root = Middleman::Application.root
     file_path = File.join(root, 'source',"#{name}.xml")
