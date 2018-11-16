351863b9ac7b710106762b72261e350269cc3446
Andrea Szöllössi <andrea.szollossi@digital.cabinet-office.gov.uk>
2018-11-16 18:39:11 +0000

    Fix image rendering
    `.svg` is the recommended file format for images, so that's what we should use.
    
    *Problem*
    
    * `.svg` doesn't render when deploying to Paas after the script runs `bundle exec middleman build`
    * `.svg` renders locally when running `bundle exec middleman server`
    
    *Solution*
    
    * removed `.svg` from the list of ignored formats in `config.rb`
    * put `saml-message-flow.svg` in `source/images` to mirror the default Middleman instalation
    * used the Middleman helper method to insert images
    
    See also: https://trello.com/c/99hTlBYb/50-put-content-in-message-flows-section

--- config.rb
+++ config.rb
@@ -4,6 +4,6 @@ GovukTechDocs.configure(self, livereload: { js_host: "localhost" })
 require "lib/snippet_helpers"
 helpers SnippetHelpers
 
-%w{xsd xml svg plantuml}.each do |ext|
+%w{xsd xml plantuml}.each do |ext|
   ignore "**/*.#{ext}"
 end
